13b13bc5b01657d78e2c204c5a0ce52db4b2cc74
zlqcos <Zhang_Lian_Qun@outlook.com>
2024-04-12 16:46:55 +0800

    第一个三角形 VAO 与EBO

--- src/main.cpp
+++ src/main.cpp
@@ -34,13 +34,28 @@ int main()
         添加渲染设置
  */
      Vector3f Vertices[4];
-    Vertices[0] = Vector3f(0.0, 0.0, 0.0f);
+    Vertices[0] = Vector3f(0.5f,  0.5f, 0.0f);
+    Vertices[1] = Vector3f( 0.5f, -0.5f, 0.0f);
+    Vertices[2] = Vector3f(-0.5f, -0.5f, 0.0f);
+    Vertices[3] = Vector3f(-0.5f,  0.5f, 0.0f);
 
-    unsigned int VBO;
+
+     unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,  // first Triangle
+        1, 2, 3   // second Triangle
+    };
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
-
+    glBufferData(GL_ARRAY_BUFFER,4* sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6*sizeof(unsigned int), indices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
    
     while (!glfwWindowShouldClose(win.getWindow()))
     {
@@ -48,7 +63,9 @@ int main()
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-       
+        shader.use();
+        glBindVertexArray(VAO);
+        glDrawElements(GL_POINTS, 6, GL_UNSIGNED_INT, 0);
      
        
         glfwSwapBuffers(win.getWindow());
