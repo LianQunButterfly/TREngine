d957a4308789e658c6acdc47400ee938341c48a5
zlqcos <Zhang_Lian_Qun@outlook.com>
2024-04-12 21:34:32 +0800

    添加texture （但是好像有问题）

--- shaderFile/fragmet.glsl
+++ shaderFile/fragmet.glsl
@@ -1,8 +1,12 @@
 #version 330 core
-layout (location = 0) in vec3 aPos;
 out vec4 FragColor;
+
+in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform sampler2D ourTexture;
 
 void main()
 {
-    FragColor = vec4(1.0,1.0,1.0, 1.0);
+    FragColor = texture(ourTexture, TexCoord);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,24 @@
 #include "math3d.h"
 #include <iostream>
 #include <fstream>  
-
+#include "model.h"
 #include "TRWindows.h"
 #include "glshader.h"
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+GLVector vertices[] = {
+//     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
+    GLVector(0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f),   // 右上
+    GLVector( 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f),   // 右下
+    GLVector(-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f),   // 左下
+    GLVector(-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f)    // 左上
+};
+int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+};
+TREngine::model mymodel = TREngine::model(vertices,4,indices,6);
 
-// settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
 
 int main()
 {
@@ -29,45 +38,26 @@ int main()
         return -1;
     }
     
-     TREngine::GLShader shader(vpath,fpath);
+     TREngine::GLShader shader =TREngine::GLShader(vpath,fpath);
 /* 
         添加渲染设置
  */
-     Vector3f Vertices[4];
-    Vertices[0] = Vector3f(0.5f,  0.5f, 0.0f);
-    Vertices[1] = Vector3f( 0.5f, -0.5f, 0.0f);
-    Vertices[2] = Vector3f(-0.5f, -0.5f, 0.0f);
-    Vertices[3] = Vector3f(-0.5f,  0.5f, 0.0f);
 
 
-     unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,  // first Triangle
-        1, 2, 3   // second Triangle
-    };
-    unsigned int VBO, VAO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER,4* sizeof(Vertices), Vertices, GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6*sizeof(unsigned int), indices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-   
+
+
+
+     mymodel.load_texture("container.jpg");
+     mymodel.set_data();
+     
     while (!glfwWindowShouldClose(win.getWindow()))
     {
 
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        shader.use();
-        glBindVertexArray(VAO);
-        glDrawElements(GL_POINTS, 6, GL_UNSIGNED_INT, 0);
-     
-       
+
+        mymodel.draw(shader);
         glfwSwapBuffers(win.getWindow());
         glfwPollEvents();
     }
